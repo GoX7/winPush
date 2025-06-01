566381174a350bd41070f9652aabf88f27b5c04c
Ilya <maslennikovila82@gmail.com>
2025-06-01 21:14:09 +0300

    Delete winpush directory

--- winpush/error.go
+++
@@ -1,9 +0,0 @@
-package winpush
-
-import "errors"
-
-var (
-	ErrExecuteToast error = errors.New("winPush: error execute toast")
-	ErrCreateFile   error = errors.New("winPush: error create file for xml push")
-	ErrReadXML      error = errors.New("winPush: error read xml for push")
-)
